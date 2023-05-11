09ee15ca52a236837951536d18cfe7d71b3e4812
liuy <1677712423@qq.com>
2023-05-11 11:17:35 +0800

    注册登录

---
+++ src/main/java/com/foottime/receptiodesk/dto/UserRegistration.java
@@ -0,0 +1,28 @@
+package com.foottime.receptiodesk.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ApiModel(value = "UserRegistration", description = "用户注册信息")
+public class UserRegistration {
+    @ApiModelProperty(value = "用户名字")
+    private String uname;
+
+    @ApiModelProperty(value = "用户密码")
+    private Integer upwd;
+
+    @ApiModelProperty(value = "用户昵称")
+    private String unickname;
+
+    @ApiModelProperty(value = "用户性别(1:男 2：女 3：保密)")
+    private Integer usex;
+
+    @ApiModelProperty(value = "用户生日")
+    private LocalDate ubirthday;
+}
--- src/main/java/com/foottime/receptiodesk/service/impl/UserService.java
+++ src/main/java/com/foottime/receptiodesk/service/impl/UserService.java
@@ -1,10 +1,18 @@
 package com.foottime.receptiodesk.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.foottime.common.CommonResult;
+import com.foottime.receptiodesk.dto.UserRegistration;
 import com.foottime.receptiodesk.entity.User;
 import com.foottime.receptiodesk.mapper.UserMapper;
 import com.foottime.receptiodesk.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +25,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    UserMapper userMapper;
+
+
+    @Override
+    public User login(String uname, Integer upwd) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda()
+                .eq(User::getUname,uname)
+                .eq(User::getUpwd,upwd);
+        User one = getOne(queryWrapper);
+        return one;
+    }
+
+    @Override
+    public boolean register(UserRegistration userRegistration) {
+        String uname = userRegistration.getUname();
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.lambda().eq(User::getUname,uname);
+        User one = getOne(wrapper);
+        if (one==null){
+            User user = new User();
+            BeanUtils.copyProperties(userRegistration,user);
+            int insert = userMapper.insert(user);
+            return insert > 0;
+        }
+        return false;
+    }
 }
