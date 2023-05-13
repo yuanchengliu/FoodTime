7d9e1306b5d74f483310742f0998700fa2f548de
FourFlavors <3475283019@qq.com>
2023-05-13 16:58:24 +0800

    变更2

---
+++ src/main/java/com/foottime/receptiodesk/mapper/SkuMapper.java
@@ -0,0 +1,16 @@
+package com.foottime.receptiodesk.mapper;
+
+import com.foottime.receptiodesk.entity.Sku;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 商品表SKU Mapper 接口
+ * </p>
+ *
+ * @author receptiodesk
+ * @since 2023-05-13
+ */
+public interface SkuMapper extends BaseMapper<Sku> {
+
+}
